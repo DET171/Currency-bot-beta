fae2db7fd26e64def7c732cd34620585ecc09cd4
Dusty <mrgeek484@gmail.com>
2021-08-10 10:12:28 +0800

    lol

--- commands/grind/use.js
+++ commands/grind/use.js
@@ -5,16 +5,16 @@ module.exports = {
 	name: 'use',
 	description: 'use an item you own',
 	args: true,
-  usage: '<item>',
+	usage: '<item>',
 	execute(message, args) {
-    const item = args.join(' ');
-    const inv = db.get(`${message.author.id}.inv`);
-    if(item == 'ticket' || item == '🎟 Ticket') {
-      if(!inv.hasOwnProperty('🎟 Ticket')) {
-        return message.channel.createMessage(`${message.author.mention}, you don't have a 🎟 Ticket!`)
-      }
-      use.loot(message.author.id, 5, 20, '💵 Cash', '🎟 Ticket', 30);
-      message.channel.createMessage(`${message.author.mention}, you successfully used a 🎟 Ticket!`)
-    }
-  }
-}
+		const item = args.join(' ');
+		const inv = db.get(`${message.author.id}.inv`);
+		if(item == 'ticket' || item == '🎟 Ticket') {
+			if(!inv.hasOwnProperty('🎟 Ticket')) { // eslint-disable-line
+				return message.channel.createMessage(`${message.author.mention}, you don't have a 🎟 Ticket!`);
+			}
+			use.loot(message.author.id, 5, 20, '💵 Cash', '🎟 Ticket', 30);
+			message.channel.createMessage(`${message.author.mention}, you successfully used a 🎟 Ticket!`);
+		}
+	},
+};
